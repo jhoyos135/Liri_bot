27b066675e7f119822445b600ff6a5f85aa4cd08
Julian Hoyos <jhoyos135@gmail.com>
2019-02-15 14:02:41 -0500

    added movie function and improve spotify function

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
 // console.log('this is loaded');
 
-exports.spotifyKeys = {
+exports.spotify_keys = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
-  };
+};
+
+exports.movies_keys = {
+  key: process.env.MOVIE_KEY
+}
--- liri.js
+++ liri.js
@@ -3,42 +3,74 @@ var keys = require("./keys.js");
 var axios = require('axios');
 var Spotify = require('node-spotify-api');
 var fs = require('fs');
-var input = process.argv;
-var action = input[2];
-var inputs = input[3];
+var action = process.argv[2];
+var result = process.argv[3];
+
+
 
 switch (action) {
 
 	case "spotify-this-song":
-	spotify(inputs);
+	spotify(result);
 	break;
 
 	case "movie-this":
-	movie(inputs);
-	break;
+	movie(result);
+    break;
+    
+    case "concert-this":
+    concert(result);
+    break;
 
 	case "do-what-it-says":
 	doit();
 	break;
 };
 
-function spotify(inputs){
+// spotify 
+function spotify(result){
 
-	let spotify = new Spotify(keys.spotifyKeys);
-		if (!inputs){
-        	inputs = 'The Sign';
-    	}
-		spotify.search({ type: 'track', query: inputs }, (err, data) => {
-            
+	let spotify = new Spotify(keys.spotify_keys);
+		if (!result){
+        	result = "the sign ace of base";
+        }
+
+		spotify.search({ type: 'track', query: result }, (err, data) => {
+
 			if (err){
 	            console.log('Error occurred: ' + err);
 	            return;
             }
+            // console.log(data.tracks.items);
             
 	        let songInfo = data.tracks.items;
-	        console.log("Artist(s): " + songInfo[0].artists[0].name);
-	        console.log("Song Name: " + songInfo[0].name);
-	        console.log("Preview Link: " + songInfo[0].preview_url);
-	        console.log("Album: " + songInfo[0].album.name);
+	        console.log(`Artist(s): ${songInfo[0].artists[0].name}`);
+            console.log(`Song Name: ${songInfo[0].name}`);
+	        console.log(`Preview Link: ${songInfo[0].preview_url}`);
+	        console.log(`Album: ${songInfo[0].album.name}`);
 	});
 }
+
+//movie OMDB
+
+function movie(result) {
+    if (!result){
+        result = 'Mr Nobody';
+    }
+
+    let movie_url = `http://www.omdbapi.com/?t=${result}&y=&plot=short&apikey=40e9cece`;
+    
+    axios.get(movie_url).then((res) => {
+        let data = res.data;
+
+            console.log("Title: " + data.Title);
+		    console.log("Release Year: " + data.Year);
+		    console.log("IMDB Rating: " + data.imdbRating);
+		    console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
+		    console.log("Country: " + data.Country);
+		    console.log("Language: " + data.Language);
+		    console.log("Plot: " + data.Plot);
+		    console.log("Actors: " + data.Actors);
+		
+    })
+};
