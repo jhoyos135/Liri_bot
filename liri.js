c852667c749007e47a261020ac499601f0063882
Julian Hoyos <jhoyos135@gmail.com>
2019-02-15 21:09:10 -0500

    finish logger functionality

--- liri.js
+++ liri.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var action = process.argv[2];
 var result = process.argv[3];
 let moment = require('moment');
+ 
 
 switch (action) {
 
@@ -23,7 +24,7 @@ switch (action) {
     break;
 
 	case "do-what-it-says":
-	doit();
+	doIt();
 	break;
 };
 
@@ -43,11 +44,19 @@ function spotify(result){
             }
             // console.log(data.tracks.items);
             
-	        let songInfo = data.tracks.items;
-	        console.log(`Artist(s): ${songInfo[0].artists[0].name}`);
-            console.log(`Song Name: ${songInfo[0].name}`);
+            let songInfo = data.tracks.items;
+            let artist = songInfo[0].artists[0].name;
+            let song = songInfo[0].name;
+            let album = songInfo[0].album.name;
+
+	        console.log(`Artist(s): ${artist}`);
+            console.log(`Song Name: ${song}`);
 	        console.log(`Preview Link: ${songInfo[0].preview_url}`);
-	        console.log(`Album: ${songInfo[0].album.name}`);
+            console.log(`Album: ${album}`);
+            
+            //logs
+            logger(artist + ' ' + song + ' ' + album)
+            
 	});
 };
 
@@ -69,7 +78,11 @@ function movie(result) {
 		    console.log("Country: " + data.Country);
 		    console.log("Language: " + data.Language);
 		    console.log("Plot: " + data.Plot);
-		    console.log("Actors: " + data.Actors);
+            console.log("Actors: " + data.Actors);
+            
+            //logs
+            logger(data.Title + ' ' + data.Year + ' ' + data.Actors+ ' ' + data.Plot);
+
 		
     });
 };
@@ -82,7 +95,7 @@ function concert(result) {
 
     let concert_url = `https://rest.bandsintown.com/artists/${result}/events?app_id=codingbootcamp`;
 
-    axios.get(concert_url).then((res) => {
+    axios.get(concert_url).then( (res) => {
 
         let data = res.data;
         for(let i in data) {
@@ -98,17 +111,19 @@ function concert(result) {
             console.log(`Date: ${new_date}`);
             console.log(`<-------------------------------->`);
 
+            logger(new_date + ' ' + venue_name + ' ' + venue_country + ' ' + venue_city);
+     
         };
     });
 };
 
 // do what it says
-function doit() {
+function doIt() {
 	fs.readFile('random.txt', "utf8", (error, data) => {
 
 		if (error) {
     		return console.log(error);
-  		}
+  		};
 
 		let dataArr = data.split(",");
 
@@ -121,8 +136,16 @@ function doit() {
 		} else if(dataArr[0] === "movie-this") {
 			let movie_this = dataArr[1].slice(1, -1);
 			movie(movie_this);
-		} 
-		
+		}
   	});
+};  
 
+//logger  
+function logger(args) {
+    fs.appendFileSync('log.txt', `
+    ${moment(new Date()).format('MMM Do YY')}
+    -- ${args}
+`, function(err) {
+        if(err) throw err;
+    });
 };
